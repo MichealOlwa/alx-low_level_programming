83854ff663a03e51c7cc22e6819f1445b4946eef
Olwa Okaka <olwamichealokaka@gmail.com>
2022-09-08 15:21:06 -0700

    tasks corrected

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,15 +10,15 @@ int main(void)
 {
 	char a_character;
 	int a_integer;
-	float a_float;
 	long a_long;
 	long long a_longlong;
+	float a_float;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(a_character));
 	printf("Size of int: %lu byte(s)\n", sizeof(a_integer));
-	printf("Size of float: %lu byte(s)\n", sizeof(a_float));
 	printf("Size of long: %lu byte(s)\n", sizeof(a_long));
 	printf("Size of long long: %lu byte(s)\n", sizeof(a_longlong));
-
-	return(0);
+        printf("Size of float: %lu byte(s)\n", sizeof(a_float));
+	
+	return (0);
 }
