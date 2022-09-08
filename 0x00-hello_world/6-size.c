b31e7e00997c70bcf71b470600ad8dd2307c656e
Olwa Okaka <olwamichealokaka@gmail.com>
2022-09-08 14:14:50 -0700

    till task5

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Entrypoint
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a_character;
+	int a_integer;
+	float a_float;
+	long a_long;
+	long long a_longlong;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(a_character));
+	printf("Size of int: %lu byte(s)\n", sizeof(a_int));
+	printf("Size of float: %lu byte(s)\n", sizeof(a_float));
+	printf("Size of long: %lu byte(s)\n", sizeof(a_long));
+	printf("Size of long long: %lu byte(s)\n", sizeof(a_longlong));
+
+	return(0);
+}
