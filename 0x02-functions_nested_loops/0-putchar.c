42f74a8ae6ab3d169264285dff3c9996f3d901bf
Olwa Okaka <olwamichealokaka@gmail.com>
2022-09-14 16:37:17 -0700

    step 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,13 @@
  *
  * Return: Always (Sucess)
  */
+
 int main(void)
 {
 	char text[9] = "_putchar";
 	int i = 0;
 
-	for (i = 0; i < 8; i++);
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(text[i]);
 	}
